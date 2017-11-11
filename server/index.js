7401a7bc92cf5ec905f5d6905c15c96b7a2990f0
Nattanich <nattanich27@gmail.com>
2017-11-11 12:05:50 +0700

    mook

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+let students = [
+  { id: 1, name: 'Mook', email: 'nattanich27@gmail.com'},
+  { id: 2, name: 'Nattanich', email: 'mook@gmail.com'}
+]
+
+app.post('/students', (req, res) =>{
+   let student = req.body
+   students.push(student)
+   res.json(student)
+})
+
+app.get('/students', (req, res) => {
+  res.json(students)
+})
+
+app.get('/greeting',(req,res) => {
+  res.json({ message: 'Hello' })
+})
+
+
+module.exports = app
